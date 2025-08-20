dcc05ca7e4629d5cbc52f84adc40fab5c3ad4797
Chahana05712 <chahanapokhrel08@gmail.com>
2025-08-20 15:14:41 +0545

    feat: testing

--- src/About.jsx
+++ src/About.jsx
@@ -11,7 +11,7 @@ function About() {
           </div>
           <div className='col-lg-6 '>
             <h2 className='fw-bold'>Allbirds ReRun™</h2>
-            <h3 className='mb-3'>A New Way To Get Old Favorites</h3>
+            <h3 className='mb-3 p-0'>A New Way To Get Old Favorites</h3>
             <p>Allbirds ReRun™ is a marketplace where you can shop slightly imperfect and gently used products, extending their life and lowering our impact on the planet. Allbirds has partnered with Trove, a company that specializes in circular commerce, to run Allbirds ReRun™ on our behalf.</p>
 
 
